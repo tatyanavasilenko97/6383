00e0c4487b7603ea39ced4e62671ada22d4f9e41
katierm <miheevakate98@gmail.com>
2017-10-09 16:53:46 +0300

    code lab1 redone

--- LR1/mikheyeva/lab1.cpp
+++ LR1/mikheyeva/lab1.cpp
@@ -1,183 +1,91 @@
-//
-// Лабораторная работа №1
-// lab1.cpp
-// Михеева Екатерина, группа 6383
-// 16.09.17
-// Программа для определения, является ли введенная строка вещественным //числом
-
-
-
-// вещественное_число::=	целое_число . целое_без_знака |
-//целое_число . целое_без_знакаЕцелое число |
-//целое_числоЕцелое_число
-//целое_без_знака::=цифра | цифра  целое_без_знака
-//целое_число::=целое_без_знака | + целое_без_знака | целое_без_знака
-
-
-
-//#include "stdafx.h"
-#include "iostream"
+#include<iostream>
 using namespace std;
-
-
-enum returns
+//вещественное_число::= Целое_число . целое_без_знака | Целое_число . целое_без_знакаЕцелое число | Целое_числоЕцелое_число
+//Целое_без_знака::=цифра | цифра целое_без_знака
+//целое_число::=целое_без_знака | + целое_без_знака | - целое_без_знака
+enum fail_type
 {
-    fig,
-    end_of_line,
-    Exp,
-    point,
-    fail
+
+    good,
+    u_int_expeced,
+    s_int_expeced,
+    exp_or_point_expected
 
 };
 
-
-bool sign(char c)
-{
-    if (c == '-' || c == '+') return true;
-    return false;
-
+bool digit(istream &in){
+    return isdigit(in.peek());
 }
 
-void what_fail(int points, int expon, bool INCORRECT_SYMBOL)
-{
-    if (points == 0 && expon == 1)
-    {
-        cout << "Число иррациональное" << endl;
-        return;
-    }
-    if (points == 0 && expon == 0 && INCORRECT_SYMBOL == 0) {
-        cout << "Число целое" << endl;
-        return;
-    }
-    cout << "Некорректный символ" << endl;
-    return;
+
+void read_digit(istream &in){
+    if(!digit(in))return;
+    cout<<((char)in.get());
+    read_digit(in);
 }
 
-bool real(std::istream &in, int &points, int &expon, bool &INCORRECT_SYMBOL);
-int figure(char c)
-
-{
-    if (c <= '9' && c >= '0')
-        return fig;
-    if (c == '\n')
-        return end_of_line;
-
-    if (c == 'e')
-        return Exp;
-    if (c == '.')
-
-        return point;
-    return fail;
-}
-
-bool u_integer(std::istream &in, int &points, int &expon, bool &INCORRECT_SYMBOL);
-
-bool integer(std::istream &in, int &points, int &expon, bool &INCORRECT_SYMBOL)
-{
-
-    switch (in.peek())
-    {
-    case '+':
-        in.get();
-
-        if (u_integer(in, points, expon, INCORRECT_SYMBOL) == false) return false; return true;
-        break;
-
-    case '-':
-
-        in.get();
-
-        if (u_integer(in, points, expon, INCORRECT_SYMBOL) == false) return false; return true;
-        break;
-
-    case '\n': {
-        if (points == 1 || expon == 1)return true;
-        return false;
-    }
-    default:
-        if (u_integer(in, points, expon, INCORRECT_SYMBOL) == false) return false; else return true;
-    }
-}
-bool u_integer(std::istream &in, int &points, int &expon, bool &INCORRECT_SYMBOL)
-{
-    char c = cin.peek();
-    switch (figure(c))
-    {
-        //цифра
-    case fig:
-        in.get();
-
-        u_integer(in, points, expon, INCORRECT_SYMBOL);
-        break;
-
-        //точка
-    case point:
-        if (expon == 1 || points == 1) return false;
-        points++;
-        in.get();
-        if (figure(in.peek()) != fig)
-        {
-            INCORRECT_SYMBOL = 1;
-            return false;
-        }
-        u_integer(in, points, expon, INCORRECT_SYMBOL);
-        break;
-
-        //конец строки
-    case end_of_line:
-        if (points == 1 || expon == 1)
-            return true;
-        else
-            return false;
-
-        //экспонента
-    case Exp:
-        if (expon == 1)
-        {
-            expon++;
-            return false;
-        }
-        expon++;
-        in.get();
-        if (figure(in.peek()) != fig && sign(in.peek() == false))
-        {
-            INCORRECT_SYMBOL = 1;
-            return false;
-        }
-        integer(in, points, expon, INCORRECT_SYMBOL);
-        break;
-
-    case fail:
-        INCORRECT_SYMBOL = 1;
-        return false;
-    }
-}
-bool real(std::istream &in, int &points, int &expon, bool &INCORRECT_SYMBOL)
-{
-    char c = in.peek();
-    if (c == '.' || c == 'e') {
-        INCORRECT_SYMBOL = 1;
-        return false;
-    }
-    if (integer(in, points, expon, INCORRECT_SYMBOL) == false) return false;
+bool unsigned_int(istream &in){
+    if(!digit(in))return false;
+    read_digit(in);
     return true;
 }
-int main()
-{
-    bool INCORRECT_SYMBOL = 0;
-    int points = 0, expon = 0;
-    setlocale(0, "rus");
-    cout << "Введите строку" << endl;
-    cin;
-    if (cin.peek() == '\n') {
-        cout << "Пустая строка" << endl;
-        return 0;
-    }
 
-    if (real(cin, points, expon, INCORRECT_SYMBOL) == true)
-        cout << "Число вещественное\n";
-    else what_fail(points, expon, INCORRECT_SYMBOL);
-    //system("pause");
-    return 0;
+bool signed_int(istream &in){
+    if(in.peek()=='-'||in.peek()=='+') cout<<((char)in.get());
+    return unsigned_int(in);
 }
 
+bool end_of_line(char c){
+
+    return c=='\n';
+}
+
+fail_type real(istream &in){
+  if(!signed_int(in))return s_int_expeced;
+
+    switch(in.peek()){
+        case 'e':
+
+            cout<<((char)in.get());
+            if(!signed_int(in)){
+
+               return s_int_expeced;
+            }
+            if(!end_of_line(in.peek())){
+
+                return s_int_expeced;
+            }
+            cout<<((char)in.get());
+            return good;
+        case '.':
+            cout<<((char)in.get());
+            if(!unsigned_int(in))return u_int_expeced;
+            if(end_of_line(in.peek()))return good;
+            if(in.peek()!='e')return u_int_expeced;
+            cout<<((char)in.get());
+            if(!signed_int(in))return u_int_expeced;
+            if(!end_of_line(in.peek()))return u_int_expeced;
+            std::cout<<((char)in.get());
+            return good;
+
+        default:
+
+        return exp_or_point_expected;
+
+}
+}
+void print_res(fail_type res)
+{
+    cout<<endl;
+    if (res == good) cout<<"good";
+    if (res == u_int_expeced) cout<<"fail! Ожидалось unsigned int"<<endl;
+    if (res == s_int_expeced) cout<<"fail! Ожидалось signed int"<<endl;
+    if (res == exp_or_point_expected) cout<<"fail! Ожидалось e или точка"<<endl;
+}
+
+int main(){
+   // fail_type res;
+cout<<"Введите строку"<<endl;
+       print_res(real(cin));
+    return 0;
+}
