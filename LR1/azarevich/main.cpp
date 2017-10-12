8d586719970d3c0454fdfdd876015f9d4eccc9b9
Lis <azartem@mail.ru>
2017-10-12 09:35:07 +0300

    LR1_Done_ver.2

--- LR1/azarevich/main.cpp
+++ LR1/azarevich/main.cpp
@@ -6,63 +6,35 @@
 //
 // Программа для вычисления биномиальных коэффициентов
 
-
 #include "b_k.h"
 using namespace std;
+
+
+void f(ui &m, ui &n, Binomial &koef);
 
 
 void main()
 {
 	setlocale(0, "");
-	unsigned int m, n;
-	bool isOverflow = false;
-
-	unsigned long long res = 0;
-
-	ifstream fin ("test.txt");
-	ofstream fout("out.txt");
-	
-	b_k koef;
+	Binomial koef;
+	ui m, n;
 
 	//Прогон по тестовым данным из файла test.txt
+	ifstream fin ("test.txt");
 	if ( fin.is_open() )
 	{
 		cout << "Найден файл с тестами\n\n";
-		fout << "Найден файл с тестами\n\n";
 		while ( !fin.eof() )
 		{
 			fin >> m;
 			fin >> n;
 
 			cout << "m= " <<m<<";	n= "<<n<<";	";
-			fout << "m= " <<m<<";	n= "<<n<<";	";
 
-			if ( m>n )
-			{
-				cout << "Бин. коэф.= 0" << endl;
-				fout << "Бин. коэф.= 0" << endl;
-			}
-			else 
-			{
-				if (n >= koef.Nmax) koef.more(n);
-
-				res = koef.binom(m, n);
-				if (koef.isOverFlow)
-					{
-					cout << "Бин. коэф.= "<<res << endl;
-					fout << "Бин. коэф.= "<<res << endl;
-				}
-				else 
-				{
-					cout << "Произошло переполнение\n";
-					fout << "Произошло переполнение\n";
-				}
-
-			}
+			f(m, n, koef);
 		}
-		fout <<"Прогон тестового файла завершён\n\n\n";
-		cout <<"Прогон тестового файла завершён\n\n\n";
 
+		cout <<"Прогон тестового файла завершён\n\n\n";
 		fin.close();
 	}
 
@@ -73,31 +45,19 @@ void main()
 		cout << "n= ";
 		cin	 >> n;
 
-		fout << "m= " <<m<<";	n= "<<n<<";	";
-
-		if ( m>n )
-			{
-				cout << "Бин. коэф.= 0" << endl;
-				fout << "Бин. коэф.= 0" << endl;
-			}
-			else
-			{
-				if (n >= koef.Nmax) koef.more(n);
-
-				res = koef.binom(m, n);
-				if (koef.isOverFlow)
-					{
-					cout << "Бин. коэф.= "<<res << endl;
-					fout << "Бин. коэф.= "<<res << endl;
-				}
-				else 
-				{
-					cout << "Произошло переполнение\n";
-					fout << "Произошло переполнение\n";
-				}
-			}
-		
-
+		f(m, n, koef);
 	}
+
+}
+
+
+void f(ui &m, ui &n, Binomial &koef)
+{
+	ull res = 0;
+
+	if ( koef.colculate(m, n, res) )
+		cout << "Бин. коэф.= "<<res << endl;
 	
+	else
+		cout << "Произошло переполнение\n";
 }
